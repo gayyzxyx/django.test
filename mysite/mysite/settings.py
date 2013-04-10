8d9792b598d81551fb0e63ee62e5b643560dace0
gayyzxyx <yao.rid@gmail.com>
2013-04-10 17:40:00 +0800

    asdfsadf

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,4 +1,5 @@
 # Django settings for mysite project.
+# -*- coding:utf-8 -*-
 import  os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -12,10 +13,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'laim',                      # Or path to database file if using sqlite3.
+        'NAME': 'django',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': 'root',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
@@ -116,12 +117,14 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.auth',
+    #'django.contrib.contenttypes',
+    #'django.contrib.sessions',
+    #'django.contrib.sites',
+    #'django.contrib.messages',
+    #'django.contrib.staticfiles',
+    #新应用
+    'users',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+# -*- coding:utf-8 -*-
 # Create your models here.
+#每个模型相当于一个数据库表
+class Publisher(models.Model):
+    name=models.CharField(max_length=30)
+    address=models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+#没有申明主键，那么django会生成一个自增长的主键字段，每个模型都要求有单独的主键id
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    #一对多的关系，django创建了一个额外的表（多对多连接表）来处理这种关系
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    publication_date = models.DateField()
+
+#python manage.py sqlall users，也即是上面的一对多关系。sqlall命令不会创建数据库，只是把表结构打出来，我们可以手动创建也可以管道操作python manager.py sqlall users|psql mydb
+#还有更简单的命令python manager.py syncdb(此命令是安全的，就算重复执行也不会重复创建表)
